3f102e2a6be97df23197aa4d3f900677038e6473
amrit shahi <ca@shahiamrit365.com.np>
2022-12-06 09:48:41 +0545

    added

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fcx$1_^%&-#tx-)nic==+f5g5aeh6qkv6@j9-+r*9uph=(h2k%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["shahiamrit365.com.np", "www.shahiamrit365.com.np", "localhost"]
+ALLOWED_HOSTS = ["shahiamrit365.com.np", "www.shahiamrit365.com.np", "localhost", "127.0.0.1"]
 
 
 # Application definition
