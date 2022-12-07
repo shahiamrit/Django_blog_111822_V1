a56c437fefb6337b79df6842b7d30496117f79c6
amrit shahi <ca@shahiamrit365.com.np>
2022-12-07 10:23:13 +0545

    added category

--- child_001/urls.py
+++ child_001/urls.py
@@ -6,5 +6,6 @@ app_name = 'child_001'
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/', views.blog, name='blog'),
-    path('blog/<int:id>/', views.readmore, name='readmore')
+    path('blog/<int:id>/', views.readmore, name='readmore'),
+    path('blog/cat/<int:id>/', views.cat, name='cat')
 ]
--- child_001/views.py
+++ child_001/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from . models import Blog
+from django.shortcuts import render, HttpResponse
+from . models import Blog, Category
 # Create your views here.
 
 def home(request):
@@ -15,4 +15,10 @@ def readmore(request, id):
     data = Blog.objects.get(pk=id)
     print(data)
     context = {'rd': data}
-    return render(request, 'blog/blog-read-more.html', context)    
+    return render(request, 'blog/blog-read-more.html', context)   
+
+def cat(request, id):
+    data = Blog.objects.filter(category_name_id=id)
+    data2 = Blog.objects.all()
+    context = {'rd': data, 'dta': data2}
+    return render(request, 'blog/categoryDetail.html', context)  
