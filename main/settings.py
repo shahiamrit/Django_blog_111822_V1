2847e8aad54e4e3e0ceba732e884c5f21f60038f
amrit <ca@shahiamrit365.com.np>
2023-03-17 19:39:10 +0545

    added

--- main/settings.py
+++ main/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-shahiamrit365',
-        'USER': 'django-shahiamrit365',
-        'PASSWORD': 'drZ6oucApiYk2VUl',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django-shahiamrit365',
+#         'USER': 'django-shahiamrit365',
+#         'PASSWORD': 'drZ6oucApiYk2VUl',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
