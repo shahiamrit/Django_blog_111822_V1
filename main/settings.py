b7367398e5fcec040e823516247de9d421b665bb
amrit shahi <ca.shahiamrit>
2023-03-29 04:43:32 +0000

    added

--- main/settings.py
+++ main/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +135,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MAX_IMAGE_PIXELS = int(1024 * 1024 * 1024 // 4 // 3)
+SUMMERNOTE_CONFIG = {
+ 'attachment_filesize_limit': MAX_IMAGE_PIXELS,
+  }
+
